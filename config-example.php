de33a5f39dca4429ef942a080b341a977a0941d1
hieunguyen64 <dhieu0307@gmail.com>
2020-06-17 23:19:15 +0700

    add config

---
+++ config-example.php
@@ -0,0 +1,11 @@
+<?php
+
+define("ROOT_DIR", __DIR__ );
+
+define("HOST", "");
+define("USER", "");
+define("PASSWORD", "");
+define("DB", "");
+
+define('BASE_URL','/');
+
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 // use composer autoload for dependencies
 require_once getcwd() . '/vendor/autoload.php';
 
+echo $_GET;
+
 echo phpinfo();
 
 ?>

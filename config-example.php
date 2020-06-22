064aaaf57f0839b0a301c4bc6771c1b0b858b8d3
Thien Ho <thien.hophu@gmail.com>
2020-06-22 23:03:31 +0700

    u

--- config-example.php
+++ config-example.php
@@ -4,7 +4,6 @@ define("USER_DB", '');
 define("PASS_DB", '');
 define("DB", '');
 
-define ('ROOT', dirname(__FILE__));
-define('BASE_URL', '');
-define('UPLOAD_IMG', ROOT .'');
-
+define("ROOT", dirname(__FILE__));
+define("BASE_URL", '');
+define("UPLOAD_IMG", ROOT . '');

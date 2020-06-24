29ec3556562ddc992514309f7fe9d014b1fb50aa
hieunguyen64 <dhieu0307@gmail.com>
2020-06-24 22:19:39 +0700

    Completed Product-single page (product-single.php)

--- view/subview/recommendeds_books.php
+++ view/subview/recommendeds_books.php
@@ -4,9 +4,7 @@
             <h2>highly recommendes books</h2>
             <hr>
         </div>
-
         <div class="row">
-
             <?php
             foreach ($data as $key => $value) {
             ?>
@@ -14,9 +12,9 @@
                     <div class="item">
                         <img src="<?php echo BASE_URL ?>/content/images/<?php echo $value['img'] ?>" alt="img">
                         <h3><?php echo $value['book_name'] ?></h3>
-                        <h6><span class="price">$49</span> / <a href="#">Buy Now</a></h6>
+                        <h6><span class="price"><?php echo $value['price'] ?>VNĐ</span> / <a href="#">Buy Now</a></h6>
                         <div class="hover">
-                            <a href="product-single.html">
+                            <a href="detail-<?php echo chuanHoaChuoi($value['book_name']) ?>-<?php echo $value['book_id'] ?>.html">
                                 <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
                             </a>
                         </div>
@@ -25,43 +23,6 @@
             <?php
             }
             ?>
-            <div class="col-lg-3 col-md-6">
-                <div class="item">
-                    <img src="images/img2.jpg" alt="img">
-                    <h3>How to write a book...</h3>
-                    <h6><span class="price">$19</span> / <a href="#">Buy Now</a></h6>
-                    <span class="sale">Sale !</span>
-                    <div class="hover">
-                        <a href="product-single.html">
-                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="item">
-                    <img src="images/img3.jpg" alt="img">
-                    <h3>7-day self publish...</h3>
-                    <h6><span class="price">$49</span> / <a href="#">Buy Now</a></h6>
-                    <div class="hover">
-                        <a href="product-single.html">
-                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="item">
-                    <img src="images/img4.jpg" alt="img">
-                    <h3>wendy doniger</h3>
-                    <h6><span class="price">$49</span> / <a href="#">Buy Now</a></h6>
-                    <div class="hover">
-                        <a href="product-single.html">
-                            <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span>
-                        </a>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 </section>
---
+++ view/test.php
@@ -0,0 +1,1 @@
+<h1><?php echo $data['book_name'] ?></h1>
